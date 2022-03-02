8eda52e49074258bd8ac66e536c3eeeecd4f1af1
Foued Jbali <foued.jbali_external@shadow.tech>
2022-03-02 12:44:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GO
+This is a simple `GO CLI` application to discover GO functionnalities
---
+++ helper/helper.go
@@ -0,0 +1,11 @@
+package helper
+
+import "strings"
+
+func ValidateUserInputs(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
+	isValidNAme := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+	return isValidNAme, isValidEmail, isValidTicketNumber
+}
